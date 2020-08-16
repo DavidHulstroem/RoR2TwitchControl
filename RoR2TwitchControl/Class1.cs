18ece073ad89f1409fbf6764cce29533c2f9a0e9
Davi3684 <45174060+DavidHulstroem@users.noreply.github.com>
2020-08-16 11:37:55 +0200

    Add project files.

---
+++ RoR2TwitchControl/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BepInEx;
+using RoR2;
+
+namespace RoR2TwitchControl
+{
+    [BepInDependency("com.bepis.r2api")]
+    //Change these
+    [BepInPlugin("com.DeusExMachinaInc.RoR2TwitchControl", "RoR2 Twitch Chat Control", "1.0.0")]
+    public class MyModName : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+        }
+    }
+}
